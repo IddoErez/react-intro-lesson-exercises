e4338faa8df0195fb8551c8acaba5154b417e1c2
Ido Erez <idodoerez@gmail.com>
2020-12-21 19:14:58 +0200

    Finished exercises

--- src/App.js
+++ src/App.js
@@ -12,10 +12,19 @@ class App extends Component {
   }
 
   showCompany(name, revenue) {
+    return (
+      <div id={name}>{name} makes {revenue} every year</div>
+    )
   }
 
   getClassName(temperature) {
-
+    if (temperature < 15) {
+      return "freezing"
+    } else if (temperature >= 15 && temperature < 30) {
+      return "fair"
+    } else {
+      return "hell-scape"
+    }
   }
 
   render() {
@@ -52,14 +61,14 @@ class App extends Component {
         <div className="ex-space">
           <h4 className='ex-title'>Exercise 1</h4>
           <div className="exercise" id="ex-1">
-            {/* your code here */}
+            {companies.map(c => this.showCompany(c.name, c.revenue))}
           </div>
         </div>
 
         <div className="ex-space">
           <h4 className='ex-title'>Exercise 2</h4>
           <div className="exercise" id="ex-2">
-            {/* your code here */}
+          <div id="weatherBox" class={this.getClassName(30)}></div>
           </div>
         </div>
       </div>
